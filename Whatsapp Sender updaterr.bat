6579c6462ad1e78844070156346234f60c9fd100
Aditya <87994109+estrizal@users.noreply.github.com>
2022-12-12 10:57:35 +0530

    Add files via upload
    FIXED MANY BUGS

--- Whatsapp Sender updaterr.bat
+++ Whatsapp Sender updaterr.bat
@@ -14,7 +14,7 @@ if '%errorlevel%' NEQ '0' (
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
     set params = %*:"=""
-    echo UAC.ShellExecute "Whatsapp_Sender_Update.exe", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "Whatsapp_Sender_Update2.exe", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
 
     "%temp%\getadmin.vbs"
     del "%temp%\getadmin.vbs"
